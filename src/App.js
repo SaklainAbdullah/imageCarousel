693ed4e715441f239403b2a46f57a00f94cf2c75
Saklain Abdullah <saklain.cuet.cse17@gmail.com>
2023-02-17 19:11:01 +0600

    deploy

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./index.css";
 
-function App() {
+const images = [
+  "img1.JPG",
+  "img2.JPG",
+  "img3.JPG",
+  "img4.JPG"
+];
+/*const myStyle = { 
+  "display": "block",
+  "margin-left": "auto",
+  "margin-right": "auto",
+  "width" : "50%"
+ 
+  }*/
+
+export default function App() {
+  const [current, setCurrent] = useState(0);
+
+  function nextSlide() {
+    setCurrent(current === images.length - 1 ? 0 : current + 1);
+  }
+
+  function prevSlide() {
+    setCurrent(current === 0 ? images.length - 1 : current - 1);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Tour de Saint Martin</h2>
+      <div className="slider">
+        <div className="left-arrow" onClick={prevSlide}>
+          ⬅
+        </div>
+        <div className="right-arrow" onClick={nextSlide}>
+          ⮕
+        </div>
+        {images.map(
+          (image, index) =>
+            current === index && (
+              <div key={image} className="slide">
+                <img /*style={myStyle}*/ src={image} alt="images" />
+              </div>
+            )
+        )}
+      </div>
     </div>
   );
 }
-
-export default App;
